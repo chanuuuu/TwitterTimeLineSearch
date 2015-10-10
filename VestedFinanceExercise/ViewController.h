72368f4a20cd17d6bf31ca29798ae745eefe0775
Chanikya <chanu.reds@gmail.com>
2015-10-09 17:48:15 -0700

    Initial Commit

---
+++ VestedFinanceExercise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VestedFinanceExercise
+//
+//  Created by Chanikya on 9/10/2015.
+//  Copyright © 2015 Chanikya Mandapathi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VestedFinanceExercise/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VestedFinanceExercise
+//
+//  Created by Chanikya on 9/10/2015.
+//  Copyright © 2015 Chanikya Mandapathi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
